4bc974ab43ddd1f7b5240e15df2c6442cc36ebce
darshilkadiwala <darshilkadiwala2020@gmail.com>
2022-01-22 20:44:35 +0530

    First Comiit

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
           <Route
             path="/"
             element={
-              <div class="container">
+              <div className="container">
                 <TextForm
                   heading="Enter text below to analyze"
                   themeMode={themeMode}
